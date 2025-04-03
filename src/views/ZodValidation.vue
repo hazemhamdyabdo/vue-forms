097df573868417c71781ab2513f0e12b7845a738
hazemhamdyabdo <hazemhamdy117@gmail.com>
2025-04-03 02:47:41 +0200

    initial commit

---
+++ src/views/ZodValidation.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section>
+    <h1>zod validation</h1>
+    <section>
+      <label for="name"> name</label>
+      <input type="text" id="name" />
+    </section>
+  </section>
+</template>
